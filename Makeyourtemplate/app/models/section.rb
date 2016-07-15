8e98dbe234ed859294a6c6d37c1d8eff003e8152
Rachel Romee <RachelRomee@Rachels-MacBook-Pro.local>
2016-07-15 14:32:02 +0200

    index, show and update methods in section controller

--- Makeyourtemplate/app/models/section.rb
+++ Makeyourtemplate/app/models/section.rb
@@ -1,3 +1,29 @@
 class Section < ActiveRecord::Base
-	
+
+# to show the template view
+	def index
+		@sections = Section.order(section_name: :asc)
+	end
+
+# # to get the right css attributes in the generator (h1 and p in text)
+# 	def show
+# 	end
+
+# to update the database with the user input
+	def update
+		@section = Section.find ( params[:id] )
+		@sections = Section.all
+
+		if @section.update_attributes( section_params )
+			redirect_to @sections
+		else
+			render 'index'
+	end
+
+	private
+
+	def section_params
+		params.require(:section).permit(:section_name, :css_rules)
+	end
+
 end
