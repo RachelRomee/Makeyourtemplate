26b55e1b5b5170529ba75470a57122833139c775
Rachel Romee <RachelRomee@Rachels-MacBook-Pro.local>
2016-07-15 18:19:28 +0200

    Show and hide button from generator

--- Makeyourtemplate/app/assets/javascripts/section.js
+++ Makeyourtemplate/app/assets/javascripts/section.js
@@ -1,8 +1,8 @@
 
 // when you click on div render section options
 $(document).ready(function() {
-	$( "#text-section" ).click(function() {
-		// need to change!!
-	  alert( "Render section" );
+	$( "#showtextinput" ).hide();
+	$( "#text" ).click(function() {
+		$( "#showtextinput" ).show();
 	});
 });
