ef8f3c1fd48f073322b4f66693c51d74fde27d0e
YOUR NAME <YOUR@EMAIL.com>
2016-07-16 16:13:20 +0530

    Add font buttons

--- Makeyourtemplate/app/assets/javascripts/section.js
+++ Makeyourtemplate/app/assets/javascripts/section.js
@@ -17,6 +17,27 @@ function text_select_h1() {
 }
 
 
+var text_h1_color;
+
+function textinput_h1_color() {
+    var text_h1_color = document.getElementById("textinput_h1_color").style.backgroundColor;
+    console.log(text_h1_color);
+    return text_h1_color;
+}
+
+var text_h1_fontFamily;
+
+function textinput_h1_fontFamily() {
+    var text_h1_fontfamily = document.getElementById("textinput_h1_fontfamily").style.fontFamily;
+    console.log(text_h1_fontfamily);
+    return text_h1_fontfamily;
+}
+
+
+var test = ".text h1 {color:" + text_h1_color + " fontFamily:" + text_h1_fontfamily + "} ";
+
+console.log(test);
+
 // SUBMITBUTTON
 function submitUserInput() {
 	// send user data to database
