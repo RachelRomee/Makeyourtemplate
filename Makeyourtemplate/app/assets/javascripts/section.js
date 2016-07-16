51a697dd7e7c01a237ca52887f5e068cc361dbe3
Rachel Romee <RachelRomee@Rachels-MacBook-Pro.local>
2016-07-16 14:00:54 +0200

    Make string of h1 color to give to database

--- Makeyourtemplate/app/assets/javascripts/section.js
+++ Makeyourtemplate/app/assets/javascripts/section.js
@@ -1,3 +1,5 @@
+var text_db;
+var text_h1_color = "pink";
 
 // when you click on div render section options
 // TEXT SECTION
@@ -10,38 +12,40 @@ $(document).ready(function() {
 		$( "#text_h1input" ).show();
 		$( "#text_pinput" ).show();
 	});
-
-var test_database;
-	// test database
-	var text_h1_color = "pink";
-	var test_database = ".text h1 {color:" + text_h1_color + "}"
-	console.log(test_database);
-	return test_database;
 });
 
-// selected
+// element select buttons
 function element_select(el, id) {
+	$('#'+id).addClass('activeselect').siblings().removeClass('activeselect');
+}
 
-	$('#'+id).addClass('active').siblings().removeClass('active');
+// get user input
+function colorInput(color) {
+	// get html element to style
+	var el = document.getElementsByClassName("activeselect")[0].innerHTML;
+	text_h1_color = color;
+}
 
 
-	// keep selected until click on other or reload
+// make string of user input
+function createCssString() {
+	text_db = ".text h1 {color:" + text_h1_color + "}";
+	console.log(text_db);
+	submitUserInput(text_db);
 }
 
 // SUBMITBUTTON
-function submitUserInput() {
+function submitUserInput(db) {
 	// send user data to database
-		$.ajax({
-	  type: "POST",
-	  url: "/css",
-	  data: { css_rules: test_database }
-	})
+	// 	$.ajax({
+	//   type: "POST",
+	//   url: "/sections/" + $(this).attr("id"),
+	//   data: { css_rules: text_db }
+	// })
 	// update section.css_rules
-	 location.reload();
+	//  location.reload();
+	 console.log(db);
 }
-
-
-
 
 //
 // BENBENBENBENBEN
